d93782097dddde8d8ec43824366b01ca8c18fd96
yamada007 <54067593+yamada007@users.noreply.github.com>
2020-01-07 17:09:27 +0100

    Add comments to app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let todoList = [];
 let todoId = 1;
 
+
+// Funktion som loopar igenom todoList arrayen och skriver ut innehållet på sidan
 function renderTodo() {
   const todoListElement = document.querySelector("#todo-list");
 
@@ -19,9 +21,11 @@ function renderTodo() {
   });
 
   todoListElement.innerHTML = todoOutput;
-  console.log(todoList);
 }
 
+// Funktion som tar bort en ToDo från todoList arrayen
+// Parameter 1 = idet på ToDo som ska tas bort
+// Parameter 2 = callback funktionen som ska köras
 function removeTodo(id, callback) {
   const modal = confirm("Är du säker på att du vill ta bort denna task?");
   if (modal) {
@@ -34,6 +38,9 @@ function removeTodo(id, callback) {
   callback();
 }
 
+// Funktion som redigerar titeln på en ToDO i todoList arrayen
+// Parameter 1 = event handlern som ger mig tillgång till den uppdaterade datan som användaren skickar med
+// Parameter 2 = idet på ToDo som ska redigeras
 function updateTodo(event, id) {
   event.preventDefault();
   const newTitle = event.target.lastElementChild.value;
@@ -52,6 +59,9 @@ function updateTodo(event, id) {
   renderTodo();
 }
 
+// Funktion som lägger till en ToDo i todoList arrayen
+// Parameter 1 = titeln på ToDo
+// Parameter 2 = callback funktionen som ska köras
 function addTodo(todo, callback) {
   todoList.splice(0, 0, {
     id: todoId,
@@ -61,11 +71,15 @@ function addTodo(todo, callback) {
   callback();
 }
 
-document.querySelector(".input-form").addEventListener("submit", event => {
-  event.preventDefault();
-  let inputValue = event.target.lastElementChild.value;
+// Eventlistener som lyssnar efter en submit på elementet med klassen .input-form
+document.querySelector(".input-form").addEventListener("submit", e => {
 
+  // Förhindrar refresh på submit
+  e.preventDefault();
+  let inputValue = e.target.lastElementChild.value;
+
+  // Ternary operator (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator)
+  // En enkel if-sats
   inputValue ? addTodo(inputValue, renderTodo) : null;
-
-  event.target.lastElementChild.value = "";
+  e.target.lastElementChild.value = "";
 });
